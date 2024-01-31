9a345128eeb7a1fd035dbce218d6950e900dee8c
himi-patel <himipatel9152@gmail.com>
2024-01-31 17:31:03 +0530

    add new files

---
+++ java/hello.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class hello
+{
+
+    public static void main(String[]args)
+    {
+        String a;
+        System.out.println("enter the name");
+        Scanner s=new Scanner(System.in);
+        a=s.nextLine();
+            System.out.println(a);
+        }
+}
